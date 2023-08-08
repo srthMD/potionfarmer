952e8af341fef462985ca682d13869ee2b905b11
srth <chillsaltyboi@gmail.com>
2023-08-08 12:06:06 -0400

    a

--- src/main/java/ro/srth/potionfarmer/item/Pomegrenade.java
+++ src/main/java/ro/srth/potionfarmer/item/Pomegrenade.java
@@ -6,7 +6,6 @@ import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.Snowball;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SnowballItem;
 import net.minecraft.world.level.Level;
@@ -22,6 +21,7 @@ public class Pomegrenade extends SnowballItem {
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
         ItemStack itemstack = pPlayer.getItemInHand(pHand);
         pLevel.playSound((Player)null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.SNOWBALL_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
+        pPlayer.getCooldowns().addCooldown(this, 10);
         if (!pLevel.isClientSide) {
             PomegrenadeEntity pomegrenade = new PomegrenadeEntity(pLevel, pPlayer);
             pomegrenade.setItem(itemstack);
